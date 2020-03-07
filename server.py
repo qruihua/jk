c7c5011d523285c4092c70e234eb7b0d5a899ea2
qruihua <38272307+qruihua@users.noreply.github.com>
2020-03-07 20:45:58 +0800

    Update server.py

--- server.py
+++ server.py
@@ -2,5 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return "index"
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=5001)
+    app.run(host="0.0.0.0",port=5000)
